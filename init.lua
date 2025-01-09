821f9a89807dbe5c847002633e2fc0e972d46068
Hamish Taylor <Hamisht11@gmail.com>
2025-01-09 19:35:22 +1300

    stash

--- init.lua
+++ init.lua
@@ -14,28 +14,3 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correctrequire
 require('lazy').setup('plugins')
 require('scripts')
-local rustaceanvim = require("rustaceanvim")
-
-rustaceanvim.setup({
-  dap = {
-    adapter = {
-      type = "executable",
-      command = "/path/to/codelldb",
-      name = "rt_lldb",
-    },
-    configurations = {
-      {
-        name = "Rust",
-        type = "rt_lldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-        runInTerminal = false,
-      },
-    },
-  },
-})
